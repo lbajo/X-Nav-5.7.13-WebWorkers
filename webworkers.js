2be7bfb84923193d352e76783c07680be82aafff
Lorena <lbajo9@gmail.com>
2017-03-09 20:20:10 +0100

    WebWorkers

---
+++ webworkers.js
@@ -0,0 +1,15 @@
+
+this.addEventListener('message', function(e) {
+  var data = e.data;
+  var n = 1;
+  primelist = "";
+  search: while (n<data) {
+    n += 1;
+    for (var i = 2; i <= Math.sqrt(n); i += 1)
+      if (n % i == 0)
+        continue search;
+  // found a prime!
+    primelist += " " + n;
+    self.postMessage(primelist);
+  }
+}, false);
